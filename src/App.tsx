c82aecfd08408edc1238159acdd57ffca1901359
Luis Mario <luis.mario.trujillo@live.com.mx>
2023-07-03 12:32:58 -0600

    Initial commit, fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import User from './types';
 
-function App() {
+const App: React.FC = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    fetch('https://gorest.co.in/public/v2/users')
+      .then(response => response.json())
+      .then(data => {
+        if (data.data) {
+          const fetchedUsers: User[] = data.data;
+          setUsers(fetchedUsers);
+        }
+      })
+      .catch(error => console.log(error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Users:</h1>
+      {users.map(user => (
+        <div key={user.id}>
+          <h2>{user.name}</h2>
+          <p>Email: {user.email}</p>
+          <p>Gender: {user.gender}</p>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
